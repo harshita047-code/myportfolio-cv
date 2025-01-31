4aaf619fe214d752a9f4daca8c6cf636033ff2c4
Harshita mehra <harshitam189@gmail.com>
2025-01-31 16:21:59 +0530

    Add files via upload

---
+++ color-changer.js
@@ -0,0 +1,28 @@
+const buttons=document.querySelectorAll('button');
+const body=document.querySelector('body');
+
+buttons.forEach(function(button) {
+    button.addEventListener('click',function(e) {
+        console.log(e)
+        console.log(e.Target)
+        if (e.target.id==='pink') {
+            body.style.backgroundColor=e.target.id;
+            
+        }
+        if (e.target.id==='red') {
+            body.style.backgroundColor=e.target.id;
+        }
+        if (e.target.id==='green') {
+            body.style.backgroundColor=e.target.id;
+        }
+        if (e.target.id==='yellow') {
+            body.style.backgroundColor=e.target.id;
+        }
+        if (e.target.id==='blue') {
+            body.style.backgroundColor=e.target.id;
+        }
+        if (e.target.id==='purple') {
+            body.style.backgroundColor=e.target.id;
+        }
+    });
+});
